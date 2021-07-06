9ddad83b1866b8ad91088f6d3de25ec72d5f6b7f
Osama Chegem <adiga_knight@hotmail.com>
2021-07-06 19:50:47 +0300

    now connecting to test cloud db & heroku:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoryModule } from './category/category.module';
 import { ItemModule } from './item/item.module';
@@ -6,7 +7,16 @@ import { StoreModule } from './store/store.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/ecommerce'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb+srv://evan:${configService.get(
+          'mongo_cloud_pass',
+        )}@mern.xupmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     StoreModule,
     CategoryModule,
     ItemModule,
